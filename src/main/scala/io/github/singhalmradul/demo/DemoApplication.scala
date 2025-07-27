85273e14d70a88145a5ac81ea44d05f22a734062
Mradul Singhal <singhalmradul@gmail.com>
2025-07-27 09:37:22 -0400

    update dependencies and add Scala formatting configuration

--- src/main/scala/io/github/singhalmradul/demo/DemoApplication.scala
+++ src/main/scala/io/github/singhalmradul/demo/DemoApplication.scala
@@ -1,7 +1,7 @@
-package io.github.singhalmradul.demo;
+package io.github.singhalmradul.demo
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
 
 @SpringBootApplication
 class Application
